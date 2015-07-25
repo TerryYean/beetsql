b65cd9a51c5628fea46251ff1e7e53b6478bdfff
Gavin-King <hiredme@163.com>
2015-07-25 23:54:36 +0800

    增加自动生成updata和getbyid接口

--- src/org/beetl/sql/SQLLoader.java
+++ src/org/beetl/sql/SQLLoader.java
@@ -3,6 +3,7 @@ package org.beetl.sql;
 import org.beetl.sql.core.SQLSource;
 
 public interface SQLLoader {
-	public SQLSource getSQL(String id);
-	
+    public SQLSource getSQL(String id);
+	public SQLSource generationUpdate(Class cls);
+	public SQLSource generationGetByid(Class cls);
 }
