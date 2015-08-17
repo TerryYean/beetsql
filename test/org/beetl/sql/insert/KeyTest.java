0003d20636b1d9236e111867ae8b40a015fb7e6b
xiandafu <xiandafu@126.com>
2015-08-17 23:02:17 +0800

    test batch update

--- test/org/beetl/sql/insert/KeyTest.java
+++ test/org/beetl/sql/insert/KeyTest.java
@@ -1,6 +1,11 @@
 package org.beetl.sql.insert;
 
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.beetl.sql.buildsql.MySqlConnectoinSource;
 import org.beetl.sql.core.ClasspathLoader;
 import org.beetl.sql.core.SQLLoader;
@@ -22,7 +27,7 @@ public class KeyTest {
 		manager = new SQLManager(new MySqlStyle(), loader, new MySqlConnectoinSource());
 	}
 
-	@Test
+//	@Test
 	public void addRole() {
 
 		Role role = new Role();
@@ -31,6 +36,32 @@ public class KeyTest {
 		KeyHolder holder = new KeyHolder();
 		script.insert(role, holder);
 		System.out.println(holder.getKey());
+		
+	}
+	
+	@Test
+	public void addRole1() {
+		
+		try{
+			Connection conn = new MySqlConnectoinSource() .getWriteConn(null);
+			PreparedStatement ps = conn.prepareStatement("update role set name =? where id=?");
+			Role role = new Role();
+			role.setId(1);
+			role.setName("ac");
+			List list = new ArrayList();
+			list.add(role);
+			list.add(role);
+			for(int i=0;i<list.size();i++){
+				Role data = (Role)list.get(i);
+				ps.setString(1, data.getName());
+				ps.setInt(2, data.getId());
+				ps.addBatch();
+			}
+			int[] result = ps.executeBatch();
+			
+		}catch(Exception ex){
+			
+		}
 		
 	}
 }
