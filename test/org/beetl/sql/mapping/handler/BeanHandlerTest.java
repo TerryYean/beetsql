6d1861acd1ae810b824eb224827dc48333d51ebd
程序猿猴 <qdlxsxj@126.com>
2015-08-18 22:09:38 +0800

    merge

--- test/org/beetl/sql/mapping/handler/BeanHandlerTest.java
+++ test/org/beetl/sql/mapping/handler/BeanHandlerTest.java
@@ -12,20 +12,8 @@
  */ 
 package org.beetl.sql.mapping.handler;
 
-import static org.mockito.Mockito.when;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import org.beetl.sql.core.mapping.ResultSetHandler;
-import org.beetl.sql.core.mapping.handler.BeanHandler;
-import org.beetl.sql.pojo.User;
-import org.junit.Assert;
-import org.junit.Before;
+import org.beetl.sql.mapping.BaseTest;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
 
 /**  
  * @ClassName: BeanHandlerTest   
@@ -33,40 +21,10 @@ import org.mockito.runners.MockitoJUnitRunner;
  * @author: suxj  
  * @date:2015年8月17日 下午4:25:20     
  */
-@RunWith(MockitoJUnitRunner.class)
-public class BeanHandlerTest {
+public class BeanHandlerTest extends BaseTest{
 	
-	@Mock
-	private User user;
-	@Mock
-	private ResultSet rs;
-	
-	@Before
-	public void setUp() throws Exception {
-		//MockitoAnnotations.initMocks(this);
-		
-//		user.setId(1);
-//		user.setName("zhangsan");
-//		user.setAge(11);
-//		user.setUserName("ZHANGSAN");
-	}
-
 	@Test
 	public void test() {
-		ResultSetHandler<User> beanHandler = new BeanHandler<User>(User.class);
-		try {
-			when(rs.getInt("id")).thenReturn(1);
-			when(rs.getString("name")).thenReturn("zhansan");
-			when(rs.getInt("age")).thenReturn(11);
-			when(rs.getString("userName")).thenReturn("ZHANGSAN");
-			
-			User _user = beanHandler.handle(rs);
-			Assert.assertEquals(user, _user);
-			
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
 		
 	}
 
